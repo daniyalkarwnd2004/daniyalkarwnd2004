55a27196e77dbfd0933a9e3fced13f137f9603c5
daniyal karwnd <karwnddaniyal16@gmail.com>
2024-03-08 18:55:08 +0330

    add def computing

--- pascal.py
+++ pascal.py
@@ -1,4 +1,9 @@
 def pascal(number):
     for i in range(0, number):
         for j in range(0, i + 1):
-            
+        #     print(binomial(i, j), " ", end=" ")
+        # print()
+
+
+def binomial(number, digit):
+    
