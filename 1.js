de8e9cf7f86de53ec88ddc7beb948d76a7eb63db
I am Daniel <162690900+daniyalkarwnd2004@users.noreply.github.com>
2024-11-11 21:07:47 +0330

    1

---
+++ 1.js
@@ -0,0 +1,12 @@
+var number1 = prompt("enter number1: ")
+var number2 = prompt("enter number2: ")
+var number1 = +number1
+var number2 = +number2
+var number3 = number1 + number2
+var number4 = number1 - number2
+var number5 = number1 * number2
+var number6 = number1 / number2
+var number7 = number1 ** number2
+console.log(number3 + " " + number4 + " " + number5 + " " + number6 + " "+ number7)
+console.log(typeof number3)
+
