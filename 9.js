0a5f9353aaccc148374a76352070501ef09e8d6f
I am Daniel <162690900+daniyalkarwnd2004@users.noreply.github.com>
2024-11-13 09:46:49 +0330

    9

---
+++ 9.js
@@ -0,0 +1,8 @@
+function number(digit1, digit2, digit3) {
+    var num4 = (digit1 + digit2 + digit3) / 3
+    alert(num4)
+}
+var num1 = Number(prompt("number1: "));
+var num2 = Number(prompt("number2: "));
+var num3 = Number(prompt("number3: "));
+number(num1, num2, num3);
