71d06eea82f8d6400a625601606e3ba50c70e7ce
I am Daniel <162690900+daniyalkarwnd2004@users.noreply.github.com>
2024-11-16 11:56:34 +0330

    11

---
+++ 11.js
@@ -0,0 +1,11 @@
+const userName = prompt("enter username: ")
+const password = prompt("enter password: ")
+if(userName.length<= 3){
+    alert("small is number username")
+}else if(password.length <= 7){
+    alert("small is number password")
+}
+else{
+    alert("welcome to web")
+}
+
