a405759dac67b041426c3df9a6320505ad6015e6
I am Daniel <162690900+daniyalkarwnd2004@users.noreply.github.com>
2024-11-20 11:02:01 +0330

    22

---
+++ 22.js
@@ -0,0 +1,7 @@
+var number1 = Number(prompt("enter number: "))
+var number2 = Number(prompt("enter number: "))
+var sum = 1
+for (var i = 0; i < number2; i ++){
+    sum = sum * number1
+}
+alert(sum)
