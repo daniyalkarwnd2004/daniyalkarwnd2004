d51899818ed06923aa92cfbf51cb4d36eb7cf607
I am Daniel <162690900+daniyalkarwnd2004@users.noreply.github.com>
2024-11-20 11:03:28 +0330

    26

---
+++ 26.js
@@ -0,0 +1,9 @@
+var my_arry = [
+    {name: "ali", family: "ahmade", age: 20, email:"ali@gmail.com"},
+    {name: "ahmd", family: "mohamde", age: 25, email:"ali@gmail.com"},
+    {name: "dani", family: "karwnd", age: 28, email:"ali@gmail.com"},
+    {name: "reza", family: "shad", age: 30, email:"ali@gmail.com"}
+]
+my_arry.forEach(function (data) {
+    console.log(data)
+})
